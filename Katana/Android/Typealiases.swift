45c2ca364ba2d8b1a0ffb0ee628773e353b9e5d0
Stefano Rodriguez <sr@bendingspoons.com>
2017-06-03 16:46:06 +0200

    Add Temp stubs to support Swift Android Build (w/o libdispatch)

--- Katana/Android/Typealiases.swift
+++ Katana/Android/Typealiases.swift
@@ -14,3 +14,43 @@ public typealias CGFloat = Double
 struct Screen {
   static var retinaScale: CGFloat = 1
 }
+
+// FIXME: Temp Stubs until `Dispatch` module is present
+struct DispatchQueue {
+  static let main = DispatchQueue()
+
+  func async(_ block: () -> ()) {
+    block()
+  }
+}
+
+// FIXME: Temp Stubs until `OperationQueue` is truly working
+class OperationQueue {
+  enum QualityOfService: Int {
+    case userInitiated
+  }
+
+  var maxConcurrentOperationCount = 1
+  var qualityOfService: QualityOfService = .userInitiated
+  var queue: [() -> ()] = []
+  var isSuspended = false {
+    didSet {
+      if !self.isSuspended {
+        for block in self.queue {
+          block()
+        }
+
+        self.queue = []
+      }
+    }
+  }
+  func addOperation(_ block: @escaping () -> ()) {
+    if self.isSuspended {
+      self.queue.append(block)
+    } else {
+      block()
+    }
+  }
+
+  init() {}
+}
